22711a70c0afbabb9fac8f42fb50cf4cad7b2aa0
vrmvikas <31568743+vrmvikas@users.noreply.github.com>
2022-11-15 18:30:59 +0530

    Create preprocessing.py

---
+++ source/preprocessing.py
@@ -0,0 +1,13 @@
+import numpy as np
+metadata = np.genfromtxt(
+        "./metadata_2013.txt",
+        delimiter="\t",
+        # skip_header=True,
+        # dtype="str",
+    )
+    # )[:,0]        #For only first column
+
+# No_of_Nodes = len(np.unique(metadata[:, 0]))
+
+_, relabels = np.unique((metadata[:,0]), return_inverse=True)     
+metadata[:,0] = relabels        #Relabels the IDs in metadata as continous integers between 0 and No_of_Nodes
