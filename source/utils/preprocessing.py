a602f634a8f7d8dc4c03421c7316f18af8d042c0
vrmvikas <31568743+vrmvikas@users.noreply.github.com>
2022-11-16 13:48:31 +0530

    Update preprocessing.py

--- source/utils/preprocessing.py
+++ source/utils/preprocessing.py
@@ -1,21 +1,34 @@
+# Following function pre-procceses the FriendshipNET dataset and returns the list of edges (Nx2 matrix).
+
 import numpy as np
-metadata = np.genfromtxt(
-        "../../data/metadata_2013.txt",
-        delimiter="\t",
-        # skip_header=True,
-        # dtype="str",
-    )
-    # )[:,0]        #For only first column
 
-# No_of_Nodes = len(np.unique(metadata[:, 0]))
+def get_friendshipnet_data():
+    metadata_orig = np.genfromtxt(
+            "./metadata_2013.txt",
+            delimiter="\t",
+            # skip_header=True,
+            # dtype="str",
+        )
+        # )[:,0]        #For only first column
 
-_, relabels = np.unique((metadata[:,0]), return_inverse=True)     
-metadata[:,0] = relabels        #Relabels the IDs in metadata as continous integers between 0 and No_of_Nodes
+    No_of_Users = len(np.unique(metadata[:, 0]))
 
+    edges_orig = np.genfromtxt(
+            "./Friendship-network_data_2013.csv",
+            delimiter=" ", 
+            # skip_header=True,
+            # dtype="str",
+        )
 
-edges = np.genfromtxt(
-        "./Friendship-network_data_2013.csv",
-        delimiter=" ",
-        # skip_header=True,
-        # dtype="str",
-    )
+    mapping, relabels = np.unique((metadata_orig[:,0]), return_inverse=True)
+    metadata = np.copy(metadata_orig)
+    metadata[:,0] = relabels        #Relabels the data 
+
+    edges = np.copy(edges_orig)
+    for j in np.unique(metadata_orig[:, 0]):
+        indices = np.where(edges[:,0] == j)
+        edges[indices, 0] = np.where(mapping == j)
+
+        indices = np.where(edges[:,1] == j)
+        edges[indices, 1] = np.where(mapping == j)
+    return No_of_Users, edges
