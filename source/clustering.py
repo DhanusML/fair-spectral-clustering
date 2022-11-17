d9dd6bb470d466301501bb5c79d0242509a6333f
Vikas Verma <vikas.verma1998@gmail.com>
2022-11-17 17:30:13 +0530

    Changes to be committed: 	modified:   source/algorithms/__pycache__/spectral_clustering.cpython-39.pyc 	modified:   source/clustering.py 	modified:   source/clustering_temp.ipynb 	modified:   source/utils/__pycache__/preprocessing.cpython-39.pyc 	modified:   source/utils/preprocessing.py

--- source/clustering.py
+++ source/clustering.py
@@ -1,1 +1,18 @@
-# 
+from algorithms.spectral_clustering import *
+from utils.preprocessing import get_friendshipnet_data
+from utils.utils import *
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+_, n, edges, _, _, gender_groups  = get_friendshipnet_data()
+
+
+
+edges = edges.astype(int)
+
+
+# clusters = normalizedSC(n, 3, edges)
+# clusters = unnormalizedSC(n, 3, edges)
+clusters = normalizedConSC(n, 2, edges, gender_groups)
+
+visualize(n, edges, clusters)
--- source/utils/preprocessing.py
+++ source/utils/preprocessing.py
@@ -1,16 +1,77 @@
-# Following function pre-procceses the FriendshipNET dataset and returns the list of edges (Nx2 matrix).
+# # Following function pre-procceses the FriendshipNET dataset and returns the list of edges (Nx2 matrix).
+
+# import numpy as np
+
+# def get_friendshipnet_data():
+#     """Returns two parameters,
+#         First: N (Number of Nodes in FriendshipNet)
+#         Second: list of edges (Nx2 Numpy Matrix)"""
+#     metadata_orig = np.genfromtxt(
+#             "../data/metadata_2013.txt",
+#             delimiter="\t",
+#             # skip_header=True,
+#             # dtype="str",
+#         )
+#         # )[:,0]        #For only first column
+
+
+#     edges_orig = np.genfromtxt(
+#             "../data/Friendship-network_data_2013.csv",
+#             delimiter=" ", 
+#             # skip_header=True,
+#             # dtype="str",
+#         )
+
+#     # mapping, relabels = np.unique((metadata_orig[:,0]), return_inverse=True)
+#     # metadata = np.copy(metadata_orig)
+#     # metadata[:,0] = relabels        #Relabels the data 
+
+#     # edges = np.copy(edges_orig)
+#     # for j in np.unique(metadata_orig[:, 0]):
+#     #     indices = np.where(edges[:,0] == j)
+#     #     edges[indices, 0] = np.where(mapping == j)
+
+#     #     indices = np.where(edges[:,1] == j)
+#     #     edges[indices, 1] = np.where(mapping == j)
+
+
+#     allnodes = np.unique(metadata_orig[:, 0])   #329
+#     mapping = np.unique(edges_orig)        #only 134 nodes are interconnected
+
+#     orphannodes = np.setdiff1d(allnodes, mapping)   #195
+
+#     edges = np.copy(edges_orig)
+#     for j in np.unique(metadata_orig[:, 0]):
+#         indices = np.where(edges[:,0] == j)
+#         edges[indices, 0] = np.where(mapping == j)
+        
+#         indices = np.where(edges[:,1] == j)
+#         edges[indices, 1] = np.where(mapping == j)
+
+#     No_of_Nodes = len(orphannodes) + len(mapping)
+#     No_of_connected_nodes = len(mapping)
+#     return No_of_Nodes, No_of_connected_nodes, edges, orphannodes
+
+
 
 import numpy as np
+import copy
 
 def get_friendshipnet_data():
-    """Returns two parameters,
-        First: N (Number of Nodes in FriendshipNet)
-        Second: list of edges (Nx2 Numpy Matrix)"""
+    """Returns the following in the same order:
+        No_of_Nodes, 
+        No_of_connected_nodes,
+        edges,
+        orphannodes,
+        gender_group,
+        reduced_group_list"""
+
+
     metadata_orig = np.genfromtxt(
             "../data/metadata_2013.txt",
             delimiter="\t",
             # skip_header=True,
-            # dtype="str",
+            dtype="str",
         )
         # )[:,0]        #For only first column
 
@@ -22,32 +83,66 @@ def get_friendshipnet_data():
             # dtype="str",
         )
 
-    # mapping, relabels = np.unique((metadata_orig[:,0]), return_inverse=True)
-    # metadata = np.copy(metadata_orig)
-    # metadata[:,0] = relabels        #Relabels the data 
-
-    # edges = np.copy(edges_orig)
-    # for j in np.unique(metadata_orig[:, 0]):
-    #     indices = np.where(edges[:,0] == j)
-    #     edges[indices, 0] = np.where(mapping == j)
-
-    #     indices = np.where(edges[:,1] == j)
-    #     edges[indices, 1] = np.where(mapping == j)
 
 
-    allnodes = np.unique(metadata_orig[:, 0])   #329
+    # edges_orig.shape    #   (668,2)
+    allnodes = np.unique(metadata_orig[:, 0].astype(int))   #329
     mapping = np.unique(edges_orig)        #only 134 nodes are interconnected
 
     orphannodes = np.setdiff1d(allnodes, mapping)   #195
 
     edges = np.copy(edges_orig)
-    for j in np.unique(metadata_orig[:, 0]):
+    metadata = np.copy(metadata_orig[:, 0]).astype(int)
+    for j in np.unique(mapping):
         indices = np.where(edges[:,0] == j)
         edges[indices, 0] = np.where(mapping == j)
         
         indices = np.where(edges[:,1] == j)
         edges[indices, 1] = np.where(mapping == j)
 
+        index = np.where(metadata_orig[:,0].astype(float) == float(j))
+        metadata[index] = np.where(mapping == j)
+
+    for j in orphannodes:
+        index = np.where(metadata_orig[:,0].astype(float) == float(j))
+        metadata[index] = np.where(orphannodes == j)
+        metadata[index] += 134
+
+    # metadata now contains relabelled data, with 
+    # values 0 to 133 indicating the nodes which 
+    # are interconnected and indicated by edges, 
+    # all other nodes are numbered 134 and onwards.
+
+    metadata_orig[:, 0] = metadata
+
+    gender_group = [[], []]
+    for i,ID in enumerate(metadata_orig[:, 0]):
+        if metadata_orig[i, 2] == 'F':
+            gender_group[1].append(int(ID))
+        elif metadata_orig[i, 2] == 'M':
+            gender_group[0].append(int(ID))
+        else:
+            gender_group[1].append(int(ID))
+
+
+    reduced_group_list = copy.deepcopy(gender_group)
+
+    for j in gender_group[0]:
+        # print(int(j))
+        if int(j) >= 134:
+            # print("removing")
+            reduced_group_list[0].remove(j)
+        
+    for j in gender_group[1]:
+        if int(j) >= 134:
+            reduced_group_list[1].remove(j)
+    
     No_of_Nodes = len(orphannodes) + len(mapping)
     No_of_connected_nodes = len(mapping)
-    return No_of_Nodes, No_of_connected_nodes, edges, orphannodes
+    
+    return No_of_Nodes, No_of_connected_nodes, edges, orphannodes, gender_group, reduced_group_list
+
+
+    # print(len(gender_group[0] + gender_group[1]))
+    # print(len(reduced_group_list[0] + reduced_group_list[1]))
+
