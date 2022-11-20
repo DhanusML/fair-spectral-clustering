62466b69322b170232e12b4e8a14ce7191fa60d9
Vikas Verma <vikas.verma1998@gmail.com>
2022-11-20 16:28:09 +0530

    Changes to be committed: 	modified:   source/clustering_temp.ipynb 	modified:   source/utils/__pycache__/preprocessing.cpython-39.pyc 	modified:   source/utils/preprocessing.py 	modified:   source/utils/preprocessing_test.ipynb

--- source/utils/preprocessing.py
+++ source/utils/preprocessing.py
@@ -4,6 +4,7 @@ import numpy as np
 import copy
 
 def get_friendshipnet_data():
+
     """Returns the following in the same order:
         No_of_Nodes, 
         No_of_connected_nodes,
@@ -14,6 +15,7 @@ def get_friendshipnet_data():
 
 
     metadata_orig = np.genfromtxt(
+            # "../../data/metadata_2013.txt",
             "../data/metadata_2013.txt",
             delimiter="\t",
             # skip_header=True,
@@ -23,6 +25,7 @@ def get_friendshipnet_data():
 
 
     edges_orig = np.genfromtxt(
+            # "../../data/Friendship-network_data_2013.csv",
             "../data/Friendship-network_data_2013.csv",
             delimiter=" ", 
             # skip_header=True,
@@ -71,20 +74,65 @@ def get_friendshipnet_data():
             gender_group[1].append(int(ID))
 
 
-    reduced_group_list = copy.deepcopy(gender_group)
+    class_group = [[], [], [], [], [], [], [], [], []]
+    for i,ID in enumerate(metadata_orig[:, 0]):
+        if metadata_orig[i, 1] == '2BIO1':
+            class_group[0].append(int(ID))        #2BIO1
+
+        elif metadata_orig[i, 1] == '2BIO2':
+            class_group[1].append(int(ID))         #2BIO2
+
+        elif metadata_orig[i, 1] == '2BIO3':
+            class_group[2].append(int(ID))         #2BIO3
+
+        elif metadata_orig[i, 1] == 'MP*1':
+            class_group[3].append(int(ID))         #MP*1
+
+        elif metadata_orig[i, 1] == 'MP*2':
+            class_group[4].append(int(ID))         #MP*2
+
+        elif metadata_orig[i, 1] == 'PSI*':
+            class_group[5].append(int(ID))         #PSI*
+
+        elif metadata_orig[i, 1] == 'PC':
+            class_group[6].append(int(ID))         #PC
+
+        elif metadata_orig[i, 1] == 'PC*':
+            class_group[7].append(int(ID))         #PC*
+
+        elif metadata_orig[i, 1] == 'MP':
+            class_group[8].append(int(ID))         #MP
+            # print("me")
+        else:
+            print(int(ID))
+
+
+
+
+    reduced_gender_group_list = copy.deepcopy(gender_group)
 
     for j in gender_group[0]:
         # print(int(j))
         if int(j) >= 134:
             # print("removing")
-            reduced_group_list[0].remove(j)
+            reduced_gender_group_list[0].remove(j)
         
     for j in gender_group[1]:
         if int(j) >= 134:
-            reduced_group_list[1].remove(j)
-    
+            reduced_gender_group_list[1].remove(j)
+
+
+
+    reduced_class_group_list = copy.deepcopy(class_group)
+    for k in range(9):
+        for j in class_group[k]:
+            if int(j) >= 134:
+                reduced_class_group_list[k].remove(j) 
+
+
+
+
     No_of_Nodes = len(orphannodes) + len(mapping)
     No_of_connected_nodes = len(mapping)
-    
-    return No_of_Nodes, No_of_connected_nodes, edges, orphannodes, gender_group, reduced_group_list
+    return No_of_Nodes, No_of_connected_nodes, edges, orphannodes, gender_group, reduced_gender_group_list, reduced_class_group_list
 
